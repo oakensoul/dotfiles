14efd7e460762424e980ce8b970184522d94aa44
Oakensoul <github@oakensoul.com>
2019-01-22 09:25:00 -0500

    Updating check env to use latest click without click_completion

--- README.rst
+++ README.rst
@@ -3,6 +3,10 @@
 
 
 cd .venv/bin
+
+For BASH
 eval "$(_CHECK_COMPLETE=source check)"
 
+For ZSH
+eval "$(_CHECK_COMPLETE=source_zsh check)"
 
--- dotfiles/check.py
+++ dotfiles/check.py
@@ -1,9 +1,10 @@
 import click
-import click_completion
 import os
 import sys
 
-click_completion.init()
+
+def get_env_vars(ctx, args, incomplete):
+    return [k for k in os.environ.keys() if incomplete in k]
 
 
 @click.group()
@@ -12,7 +13,7 @@ def main():
 
 
 @main.command(help='A command to print environment variables')
-@click.argument('env_var', type=click.Choice(os.environ.keys()))
+@click.argument('env_var', type=click.STRING, autocompletion=get_env_vars)
 def env(env_var):
     click.echo(f'Environment check:')
     click.echo(f'  Variable : {env_var}')
